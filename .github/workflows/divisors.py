4958bf25d67f2c3e65176ff7f676c66961f3cf75
Shin-Yeon-Su <yeonsu04210@gmail.com>
2024-11-08 21:56:04 +0900

    Update divisors.py

--- .github/workflows/divisors.py
+++ .github/workflows/divisors.py
@@ -2,7 +2,7 @@ import sys
 
 number = int(sys.argv[1])
 
-for i in range(1, number + 1):  
-    if number % i == 0:  
-        print(i, end=" ")  
+for i in range(1, number + 1):  # 1부터 number까지 반복
+    if number % i == 0:  # 나머지가 0이면
+        print(i, end=" ")  # 약수 출력
 print()
